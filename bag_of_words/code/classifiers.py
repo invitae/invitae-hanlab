badb3900fda387dcab0e6d8fdef4bd3b5be5f4aa
JinfengXiao <jxiaoaa@gmail.com>
2018-10-05 10:57:06 -0500

    bag of words, Oct 5

--- bag_of_words/code/classifiers.py
+++ bag_of_words/code/classifiers.py
@@ -1,9 +1,10 @@
-import pickle
+import pickle, heapq
 from itertools import compress
 import numpy as np
 from sklearn.model_selection import cross_validate
 from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 indir = "../data/processed/"
 
@@ -22,10 +23,20 @@ bow = bow[labels_pos_ind.values,:]
 bow_rownames = list(compress(bow_rownames, labels_pos_ind.values))
 labels = labels[labels_pos_ind.values]
 
-# Take the most frequent ~1000 words
-bow_colsums = (bow > 0).sum(axis=0) / len(bow_rownames)
-(bow_colsums > 0.2).sum()  # The most frequent 1155 words appear in >20% of all papers
-bow_freq = bow[:,(bow_colsums > 0.2).A1]
+# Take the topk tokens
+topk = 1000
+df = (bow > 0).sum(axis=0)
+bow = bow[:,(df > 0).A1]  # Remove words with zero occurance in the sub-corpus
+df = (bow > 0).sum(axis=0)
+tf = bow.sum(axis=0)
+idf = np.log( (len(bow_rownames) + 1) / df )
+tfidf = np.multiply(tf, idf)
+# Select tokens based on TF-IDF
+cutoff = heapq.nlargest(topk, tfidf.A1.tolist())[topk-1]
+bow_freq = bow[:,(tfidf >= cutoff).A1]
+# Select tokens based on DF
+#cutoff = heapq.nlargest(topk, df.A1.tolist())[topk-1]
+#bow_freq = bow[:,(df >= cutoff).A1]
 
 # Proportion of False labels (i.e. accuracy of an all-False classifier)
 false_prop = (labels == False).sum(axis=0) / labels.shape[0]
@@ -33,12 +44,46 @@ false_prop = (labels == False).sum(axis=0) / labels.shape[0]
 
 
 # Train and test classifiers for a specific label
-label = "family_studies"
+cv_fold = 3
+for label in ["individual_observations", "functional_experiments", "family_studies", "sequence_observations"]:
+    print("Class: " + label)
+    scoring = ["precision", "recall", "f1", "accuracy"]
+    
+    print("LinearSVC with L2:")
+    clf = LinearSVC(random_state=0, tol=1e-5)
+    cv_results = cross_validate(clf, bow_freq, labels[label], cv=cv_fold, n_jobs=-1, scoring=scoring, return_train_score=False)
+    print("  Accuracy: " + str(sum(cv_results['test_accuracy']) / cv_fold))
+    print("  Precision: " + str(sum(cv_results['test_precision']) / cv_fold))
+    print("  Recall: " + str(sum(cv_results['test_recall']) / cv_fold))
+    print("  F1: " + str(sum(cv_results['test_f1']) / cv_fold))
+    print("")
+    
+    print("LinearSVC with L1:")
+    clf = LinearSVC(dual=False, random_state=0, tol=1e-5, penalty='l1')
+    cv_results = cross_validate(clf, bow_freq, labels[label], cv=cv_fold, n_jobs=-1, scoring=scoring, return_train_score=False)
+    print("  Accuracy: " + str(sum(cv_results['test_accuracy']) / cv_fold))
+    print("  Precision: " + str(sum(cv_results['test_precision']) / cv_fold))
+    print("  Recall: " + str(sum(cv_results['test_recall']) / cv_fold))
+    print("  F1: " + str(sum(cv_results['test_f1']) / cv_fold))
+    print("")
+    
+    print("Decision Tree:")
+    clf = DecisionTreeClassifier(random_state=0, max_depth=10)
+    cv_results = cross_validate(clf, bow_freq, labels[label], cv=cv_fold, n_jobs=-1, scoring=scoring, return_train_score=False)
+    print("  Accuracy: " + str(sum(cv_results['test_accuracy']) / cv_fold))
+    print("  Precision: " + str(sum(cv_results['test_precision']) / cv_fold))
+    print("  Recall: " + str(sum(cv_results['test_recall']) / cv_fold))
+    print("  F1: " + str(sum(cv_results['test_f1']) / cv_fold))
+    print("")
+    
+    print("Random Forest:")
+    clf = RandomForestClassifier(n_estimators=100, max_depth=10, n_jobs=10)
+    cv_results = cross_validate(clf, bow_freq, labels[label], cv=cv_fold, n_jobs=-1, scoring=scoring, return_train_score=False)
+    print("  Accuracy: " + str(sum(cv_results['test_accuracy']) / cv_fold))
+    print("  Precision: " + str(sum(cv_results['test_precision']) / cv_fold))
+    print("  Recall: " + str(sum(cv_results['test_recall']) / cv_fold))
+    print("  F1: " + str(sum(cv_results['test_f1']) / cv_fold))
+    print("")
+    print("")
+    print("")
 
-clf = LinearSVC(random_state=0, tol=1e-5)
-cv_results = cross_validate(clf, bow_freq, labels[label], cv=3)
-sum(cv_results['test_score']) / len(cv_results['test_score'])
-
-clf = DecisionTreeClassifier(random_state=0, max_depth=10)
-cv_results = cross_validate(clf, bow_freq, labels[label], cv=3)
-sum(cv_results['test_score']) / len(cv_results['test_score'])
